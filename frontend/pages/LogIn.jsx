4289a966505d94c86d8267ceb6bd453ba8e941d3
Sushant-Sah1 <sushant210706@gmail.com>
2025-08-11 12:14:05 +0530

    Update LogIn.jsx
    updating backend url

--- frontend/pages/LogIn.jsx
+++ frontend/pages/LogIn.jsx
@@ -13,7 +13,7 @@ const LogIn = () => {
 
   async function handlelogin() {
     try {
-      const data = await axios.post("http://localhost:4444/api/auth/login", {
+      const data = await axios.post("https://cosmic-chat-backend.onrender.com/api/auth/login", {
         email: emailref.current.value,
         password: passwordref.current.value,
       });
