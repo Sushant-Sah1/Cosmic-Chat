0a405ff29aeccb5345ab3b3747d9780d5c9f7ffa
Sushant-Sah1 <sushant210706@gmail.com>
2025-08-11 12:14:52 +0530

    Update PrivateGroups.jsx

--- frontend/pages/PrivateGroups.jsx
+++ frontend/pages/PrivateGroups.jsx
@@ -7,7 +7,7 @@ const PrivateGroups = ({ handlejoin, joinref }) => {
   async function handleprivsearch() {
     try {
       const data = await axios.post(
-        "http://localhost:4444/api/groups/searchprivgroup"
+        "https://cosmic-chat-backend.onrender.com/api/groups/searchprivgroup"
       );
       console.log(data)
       setprivgroups(data.data);
